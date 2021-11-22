466273d0ca656bd2a6e570860ff1f96711b54b89
edumoraes <edumoraesdg@gmail.com>
2021-11-22 08:32:42 -0400

    add archive

---
+++ main.js
@@ -0,0 +1,33 @@
+/* Testimonials slider swiper */
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 2.1,
+  freeMode: true,
+  watchSlidesProgress: true,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false
+  },
+  mousewheel: {
+    enabled: true
+  }
+})
+
+/* Testimonials slider swiper */
+var swiper2 = new Swiper('.mySwiper2', {
+  slidesPerView: 1.4,
+  freeMode: true,
+  watchSlidesProgress: true,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false
+  },
+  mousewheel: {
+    enabled: true
+  }
+})
